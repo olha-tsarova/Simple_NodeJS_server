1d38ad4d347f7fb908ae09ae0c8a5d69bba8efb0
OlgaTs <alita.jet@gmail.com>
2021-07-27 19:55:18 +0300

    upd v4

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const http = require('http')
+const url = require('url')
+const { parse } = require('querystring')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
+db.once('open', function () {
   console.log('Connected')
 })
 
@@ -21,71 +26,79 @@ const todoSchema = new mongoose.Schema({
 const Todo = mongoose.model('Todo', todoSchema)
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
-  console.log(`method: ${method} url: ${url}`)
+  let urlRequest = url.parse(req.url, true)
+  console.log(urlRequest)
+  // console.log(`method: ${method} url: ${url}`)
 
   res.setHeader('Content-Type', 'application/json')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PATCH')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'POST, GET, OPTIONS, DELETE, PATCH'
+  )
 
-  if (method === 'GET' && url === '/todos') {
-    await Todo.find({})
-      .then(result => {
-        res.end(JSON.stringify(result))
-      })
+  if (req.method === 'GET' && urlRequest.pathname === '/todos') {
+    await Todo.find({}).then((result) => {
+      res.end(JSON.stringify(result))
+    })
   }
 
   if (method === 'POST' && url === '/addtodo') {
-    req.on('data', async chunk => {
+    req.on('data', async (chunk) => {
       const data = JSON.parse(chunk)
       const newTodo = new Todo(data)
-      console.log(newTodo);
+      console.log(newTodo)
       await newTodo.save((err, newTodo) => {
         if (err) {
           console.error(err)
           res.end(err)
         }
+        res.end(JSON.stringify(newTodo))
       })
     })
-
-    res.end('added to list')
   }
 
   if (method === 'DELETE' && url === '/delete') {
-    req.on('data', async chunk => {
+    req.on('data', async (chunk) => {
       const keys = JSON.parse(chunk)
-
-      const result = await Todo.deleteMany({ key: { $in: keys } })
-
-      console.log('result ++ ', result);
+      console.log(keys)
+      await Todo.deleteMany({ key: { $in: keys } }).then(
+        res.end(JSON.stringify(keys))
+      )
     })
-
-    res.end('deleted from list')
   }
 
   if (method === 'PATCH' && url === '/changestatuses') {
-    req.on('data', async chunk => {
+    req.on('data', async (chunk) => {
       const data = JSON.parse(chunk)
-
-      console.log(data);
-
-      const result = await Todo.updateMany({ key: { $in: data.keys}}, { completed: data.data.completed })
+      console.log(data)
+      await Todo.updateMany({}, { completed: data })
+      await Todo.find({}).then((result) => {
+        res.end(JSON.stringify(result))
+      })
     })
-
-    res.end('statuses updated')
   }
 
   if (method === 'PATCH' && url === '/edit') {
-    req.on('data', async chunk => {
+    req.on('data', async (chunk) => {
       const data = JSON.parse(chunk)
-      console.log(data);
+      console.log(data)
 
-      const result = await Todo.findOneAndUpdate({ key: data.key }, { title: data.title, completed: data.completed }, {new: true})
+      const result = await Todo.findOneAndUpdate(
+        { key: data.key },
+        { completed: !data.completed },
+        { new: true }
+      )
       res.end(JSON.stringify(result))
     })
+  }
 
-    // res.end('task updated')
+  if (method === 'DELETE' && url === '/clearcompleted') {
+    await Todo.deleteMany({ completed: true })
+    await Todo.find({}).then((result) => {
+      res.end(JSON.stringify(result))
+    })
   }
 
   if (method === 'OPTIONS') {
